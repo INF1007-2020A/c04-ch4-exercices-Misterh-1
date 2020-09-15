50cd8222491b6ea39c6ce04fecec6abd23d47fec
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-15 23:11:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,6 +22,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     string_split = string.split
+    print(string_split)
     new_string = ""
     for i in range (len(string)):
         if string_split[i] == 'w' :
