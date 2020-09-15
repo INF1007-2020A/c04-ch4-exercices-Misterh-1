4220baaa1de5178d643eb0968f0efbb16870002a
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-15 23:12:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,7 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    string_split = string.split
+    string_ = string.split
     print(string)
     new_string = ""
     #for i in range (len(string)):
