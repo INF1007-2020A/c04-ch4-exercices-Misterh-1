d25d87ce01d3b5c7153b5881a6d87b1fe3fda923
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-15 22:53:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    new_word = ''
+    new_word = ""
     for i in range (len(string)) :
         if i != 2 :
             new_word = 
