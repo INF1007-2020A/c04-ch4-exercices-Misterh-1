f848d3fc3f185e391f7745daee3ddfd33a4aa869
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-15 23:30:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     new_string = ""
     for i in range (len(string)):
         if string[i] == old_char :
-            new_string = 
+            new_string += 
     return new_string
     pass
 
