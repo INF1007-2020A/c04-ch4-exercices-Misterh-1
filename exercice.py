bae27a0cb2459d4bf449e82e6b4f9483d78b876d
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-16 00:05:32 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -42,7 +42,7 @@ def get_number_of_words(sentence: str, word: str) -> int:
      #print(sentence)
      #print(len(sentence)
      for i in range (len(sentence)) :
-         if 
+         if sentence[i+1] t
          if sentence[i] == sentence[i +1]:
              counter += 1
      return counter
