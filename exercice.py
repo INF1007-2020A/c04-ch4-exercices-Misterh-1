6473a4661fd7a1de733c53208f67debb5dcd411b
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-16 00:21:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -39,7 +39,7 @@ def get_number_of_char(string: str, char: str) -> int:
 def get_number_of_words(sentence: str, word: str) -> int:
      counter = 0
      sentence = sentence.split(" ")
-     sentence
+     sentence.append(" ")
      #print(sentence)
      #print(len(sentence)
      for i in range (len(sentence)) :
