cb80018a5bade5b678b7ee3bffb736972009aca5
Misterh-1 <hamidfagbemi079@gmail.com>
2020-09-16 00:00:22 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -40,8 +40,8 @@ def get_number_of_words(sentence: str, word: str) -> int:
      counter = 0
      sentence = sentence.split(" ")
      #print(sentence)
-     print(len(sentence))
-   #for i in range (len(sentence)):
+     #print(len(sentence))
+   for i in range (len(sentence)):
     #    if string[i] == char :
     #        counter +=1 
     #return counter
